855f2471de36c66342a5452bfe04b8815175ab18
mtanuan2 <mtanuan2@esolutionsgroup.ca>
2019-12-24 12:20:47 -0500

    BTN-UnitTest2 with comment

---
+++ UnitTest2.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace XUnitTestProject1
+{
+    public class UnitTest2
+    {
+        [Fact]
+        public void xunitworks()
+        {
+            Assert.True(true);
+        }
+    }
+}
